f5e12dc2af6fa947d9abe211bbf1210ababfd972
BEN-MTP <minhmanh.office.uet@gmail.com>
2022-01-19 15:40:58 +0700

    thread simple

---
+++ Java-ThreadPool/Java-Thread/src/com/manhkm/simple/RunnableSimple.java
@@ -0,0 +1,13 @@
+package com.manhkm.simple;
+
+/**
+ * @author ManhKM on 1/19/2022
+ * @project Java-Thread
+ */
+public class RunnableSimple implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("thread is running 123.....!");
+    }
+}
---
+++ Java-ThreadPool/Java-Thread/test/ThreadSimpleMain.java
@@ -0,0 +1,12 @@
+import com.manhkm.simple.ThreadSimple;
+
+/**
+ * @author ManhKM on 1/19/2022
+ * @project Java-Thread
+ */
+public class ThreadSimpleMain {
+    public static void main(String[] args) {
+        ThreadSimple threadSimple = new ThreadSimple();
+        threadSimple.start();
+    }
+}
