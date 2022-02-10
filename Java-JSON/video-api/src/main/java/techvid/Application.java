16402c7b0adb2f00163c3015f3aa6502ed5b72d7
BEN-MTP <minhmanh.office.uet@gmail.com>
2022-02-10 09:45:57 +0700

    create new project spring rest

--- Java-JSON/video-api/src/main/java/techvid/Application.java
+++ Java-JSON/video-api/src/main/java/techvid/Application.java
@@ -1,6 +1,17 @@
-package techvid;/**
- * @project video-api
+package techvid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * File entry -> dùng cho việc chạy ứng dụng của toàn project.
  * @author ManhKM on 2/10/2022
-*/
-    public class Application {
+ * @project video-api
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("Running Application - [video-api]");
+    }
 }
--- Java-JSON/video-api/src/main/java/techvid/controller/VideoController.java
+++ Java-JSON/video-api/src/main/java/techvid/controller/VideoController.java
@@ -1,6 +1,28 @@
-package techvid.controller;/**
- * @project video-api
+package techvid.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Đang xây dựng một Video API
+ * Các request mapping
+ *      localhost:8080/....
  * @author ManhKM on 2/10/2022
-*/
-    public class VideoController {
+ * @project video-api
+ */
+@RestController
+public class VideoController {
+
+    @RequestMapping("/")
+    public String index(){
+        String body = "<html>\n" +
+                "<title>Techzone-fun</title>" +
+                "<body>\n" +
+                "<h1>My First Heading</h1>\n" +
+                "<p>My first paragraph.</p>\n" +
+                "</body>\n" +
+                "</html>";
+        return body;
+    }
+
 }
