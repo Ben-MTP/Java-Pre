33314aa6c4d5bc641c8605eb5a20fd2e31a18843
BEN-MTP <minhmanh.office.uet@gmail.com>
2022-02-10 09:43:21 +0700

    update video-manager and document

---
+++ Java-JSON/video-api/src/main/java/techvid/Application.java
@@ -0,0 +1,6 @@
+package techvid;/**
+ * @project video-api
+ * @author ManhKM on 2/10/2022
+*/
+    public class Application {
+}
---
+++ Java-JSON/video-api/src/main/java/techvid/controller/VideoController.java
@@ -0,0 +1,6 @@
+package techvid.controller;/**
+ * @project video-api
+ * @author ManhKM on 2/10/2022
+*/
+    public class VideoController {
+}
