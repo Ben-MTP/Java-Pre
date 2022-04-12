c88954982cff1de5f6c08f6b54ad1638288c19db
BEN-MTP <minhmanh.office.uet@gmail.com>
2022-04-12 14:07:03 +0700

    add example for earch

---
+++ Java-Collection/for-each/src/core/forEach/ForEachOrderedExample.java
@@ -0,0 +1,6 @@
+package core.forEach;/**
+ * @project Java-Collection
+ * @author ManhKM on 4/12/2022
+*/
+    public class ForEachOrderedExample {
+}
---
+++ Java-Lambda/src/core/lambdaexpression/comparatorvslambda/ComparatorVsLambda.java
@@ -0,0 +1,6 @@
+package core.lambdaexpression.comparatorvslambda;/**
+ * @project Java-Lambda
+ * @author ManhKM on 4/12/2022
+*/
+    public class ComparatorVsLambda {
+}
