da425ac124d25d77513f12bb3722937dbf62d207
ManhKM <minhmanh.office.uet@gmail.com>
2023-02-27 08:55:59 +0700

    Update thread in Java-Runnable

--- Java-Concurrence/Java-Runnable/java-runnable/src/com/core/App.java
+++ Java-Concurrence/Java-Runnable/java-runnable/src/com/core/App.java
@@ -7,12 +7,22 @@ package com.core;
 public class App {
 
     public static void main(String[] args) {
+        Thread.currentThread().setName("Thread-main-of-java-runnable");
         System.out.println("Main thread is - " +
-            Thread.currentThread().getName());
+                Thread.currentThread().getName() + "\n");
 
-        for(int i = 0; i < 1000; i++){
-            Thread thread1 = new Thread(new RunnableImpl());
-            thread1.start();
-        }
+        Runnable runnable1 = new RunnableImpl("A", 50);
+        Thread task = new Thread(runnable1);
+        task.setName("Thread-A");
+
+        Runnable runnable2 = new RunnableImpl("B", 100);
+        Thread task1 = new Thread(runnable2);
+        task1.setName("Thread-B");
+
+        task.start();
+
+        task1.start();
+
+        System.out.println("---THE END---");
     }
 }
--- Java-Concurrence/Java-Runnable/java-runnable/src/com/core/RunnableImpl.java
+++ Java-Concurrence/Java-Runnable/java-runnable/src/com/core/RunnableImpl.java
@@ -4,10 +4,26 @@ package com.core;
  * @author ManhKM on 8/10/2022
  * @project java-runnable
  */
-public class RunnableImpl implements Runnable{
+public class RunnableImpl implements Runnable {
+
+    private String name;    // name of Runnable
+    private int count;      // count
+
+    public RunnableImpl(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
 
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName() + ", executing run() method!");
+        for (int i = 0; i < count; i++) {
+            System.out.print(name);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("\n" + Thread.currentThread().getName() + ", executing run() method!");
     }
 }
